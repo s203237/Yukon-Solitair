00630dcb64a97040a0d008b805b99fc1948bef05
Camellia <91070768+s203237@users.noreply.github.com>
2025-04-26 00:37:46 +0200

    update

--- src/game.c
+++ src/game.c
@@ -24,28 +24,6 @@ void initGame(){
 
 }
 
-// load deck from file
-bool loadDeckFromFile(const char*filename){
-    FILE*file = fopen(filename,"r");
-    if(!file)return false;// if load successful: true
-    // read file
-    char line[10]; // read line by line from file
-    Card*last=NULL; // until the last card
-    int count =0;// count the number of cards read
-
-    while(fgets(line, sizeof(line),file)){
-        if(strlen(line)<2) continue;// skip if line is too short (invalid)
-        char rank = line[0];
-        char suit = line[1];
-        Card*newCard = createCard(rank, suit, false);// create a new card (default is faceUp = false)
-        if (!deck) deck = newCard;// add card to deck list
-        else last -> next = newCard;
-        last = newCard;
-        count++;
-    }
-    fclose(file);
-    return count ==52;
-}
 
 int my_random(int n) {
     int seed = (n * 73 + 17) % 1007; //simple but sufficient for basic shuffling
